5a110c6499a37485626f483b3687a3dbad54c809
= <=>
2024-11-25 14:09:58 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  /**
+   *  const { name,value } = e.target
+   *
+   *
+   * {
+   *    email: asdsahdasda,
+   *    password: ádjasjdjas
+   * }
+   *
+   */
+
+  // không có clean up useEffect(()=>{})
+  // có clean up
+  useEffect(() => {
+    // Promise (lời hứa) -> 1. pending - 2.fulfilled
+    //                                 \ 3.rejected
+
+    // restful api
+    /**
+     * GET http://localhost:8080/api/v1/todos
+     * GET http://localhost:8080/api/v1/todos/     *
+     */
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  return (
+    <>
+      {data?.map((item) => (
+        <p key={item.id}>{item.name}</p>
+      ))}
+    </>
+  );
+}
+
+export default App;
